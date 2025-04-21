bb2c78dc025c2a0d76efceb1fe2537d525ced091
JIHOON LEE <jihoon7705@gmail.com>
2025-04-21 18:15:49 +0900

    feat(250421) : 00043-easy-exclude.ts

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,12 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T
+
+// exclude 는 한 유니온 타입에서 다른 유니온 타입에 할당 가능한 타입을 제외하는 기능 (차집합)
+// TypeScript의 유니온 타입에 대한 정규화(normalization) 과정에서 never 타입이 제거됨
+
+
+// 유사 개념 (Extract vs Exclude)
+type Animals = 'cat' | 'dog' | 'bird';
+type Pets = 'cat' | 'dog' | 'hamster';
+
+type WildAnimals = Exclude<Animals, Pets>;  // 'bird'
+type CommonPets = Extract<Animals, Pets>;   // 'cat' | 'dog'
