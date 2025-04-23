b087e8eebac680851e5bf68f756d2e4afd165bee
JIHOON LEE <jihoon7705@gmail.com>
2025-04-23 17:16:29 +0900

    feat(250423) : 00533-easy-concat.ts

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,5 @@
-type Concat<T, U> = any
+type Concat<T extends readonly any[], U extends readonly any[]> = [...T, ...U]
+
+// concat 은 두 배열을 합쳐서 하나의 배열로 만드는 함수
+
+// readonly 를 넣어줘야 as const 어서션으로 생성된 읽기 전용 튜플에도 호환할 수 있다.
